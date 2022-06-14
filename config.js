8252de41ebaf646ef1e88f550f9a4ab2f69063f6
Rullmem <107421288+Rullmem@users.noreply.github.com>
2022-06-14 17:08:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.numberowner = '6285822347348'
 global.instagram = 'https://instagram.com/sahrulwara_____'
 global.github = 'https://github.com/sahrull11'
 global.dana = '085822347348'
-global.pulsa = '085822347348,083843192208'
+global.pulsa = '085822347348\n083843192208'
 global.gopay = ''
 
 //=============『 Info Bot 』=================//
@@ -19,7 +19,7 @@ global.web = 'https://youtu.be/jOhhY7ef_qM' //ubah jadi website lu, bisa link ig
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000\nPermanen 15.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/hiruko.jpg' //ganti jadi foto bot mu
+global.fotonya1 = 'https://telegra.ph/file/images.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/hiruko.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
